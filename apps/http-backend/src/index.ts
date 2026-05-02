a8b65315e43d6c8469e275bf311ec4691fda0df3
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-05-02 22:26:48 +0200

    this is how you return jwttoken in queryparams in ws server

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import authRouter from "./routes/auth.routes";
+import { verifyToken } from "./middleware/verifyToken.middleware";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server  is healthy" });
 });
 
-app.get("/create-room", (req, res) => {
+app.get("/room", verifyToken, (req, res) => {
   res.json({
     message: "this is create-room end point",
   });
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,24 @@
 import { WebSocketServer } from "ws";
-import type { WebSocket } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws: WebSocket) {
+wss.on("connection", function connection(ws, request) {
+  const usersUrl = request.url;
+
+  if (!usersUrl) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(usersUrl.split("?")[1]);
+  const token = queryParams.get("token") || "fall-back-to-empty-string";
+  const decode = jwt.verify(token, JWT_SECRET);
+
+  if (!decode || !(decode as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
   ws.on("message", function message(data) {
     ws.send("pong");
   });
